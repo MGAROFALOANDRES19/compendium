c4291a14b5dc78e1e78fba63bc7b0c67f0d82466
root <root@localhost.localdomain>
2017-09-26 00:26:12 -0500

    Tema base para la revista compendium

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/**
+ * @file plugins/themes/default/index.php
+ *
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @brief Wrapper for default theme plugin.
+ *
+ */
+
+require_once('CompendiumThemePlugin.inc.php');
+
+return new CompendiumThemePlugin();
+
+?>
